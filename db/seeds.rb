8a7f731b645d2681bb209c0aca7a3d6d1724720c
Elta3lab <ahmedfouad2020era@gmail.com>
2018-06-12 17:12:25 +0200

    bugs 12

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,12 @@ User.create!(name:  "Ahmed Mohamed Fouad",
              password_confirmation: "12345678",
              admin: true)
 
+User.create!(name:  "Ahmed Fouad",
+             email: "elta3lab50cent@gmail.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             admin: false)
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
