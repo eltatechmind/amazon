c86007f8e20944a7bf31c3e17145fe92ac7d9652
Elta3lab <ahmedfouad2020era@gmail.com>
2018-06-18 16:22:54 +0200

    add more validations to photo.rb model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ApplicationRecord
 	has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+ 	validates_attachment_presence :image
+    validates_attachment_size :image, :less_than => 5.megabytes
+    validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 end
