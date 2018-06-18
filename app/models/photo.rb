8b06be2076440ec4e0faf2dfbfdadc5029028e14
Elta3lab <ahmedfouad2020era@gmail.com>
2018-06-18 16:45:33 +0200

    add more validations and edit size of photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,10 @@
 class Photo < ApplicationRecord
-	has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, styles: { large: "600x600", medium: "100x100", thumb: "100x100" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
  	validates_attachment_presence :image
     validates_attachment_size :image, :less_than => 5.megabytes
-    validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+    validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/jpg']
+	validates_uniqueness_of :image_file_name # this is a standard ActiveRecord validator
+    #validates_attachment_file_name :image, :matches => [/_\d+_\d+_\d+\.bin$/]
+    
 end
