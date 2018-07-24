48bf66278ec0fe07fd83a625ab33d81f43cd4d09
Elta3lab <ahmedfouad2020era@gmail.com>
2018-07-25 00:44:50 +0200

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [ :cart, :edit, :update, :show, :destroy, :order, :cart, :cancelled, :completed, :cancelorder, :removecart, :addcart, :addorder]
   before_action :correct_user,   only: [ :edit, :update, :show, :destroy]
 
-
+ #the home page
   def home
     redirect_to categories_path if current_user.present?
   end
