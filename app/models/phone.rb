eaf8723ee15daf4a19f17ba995f25bf289a1a623
Ahmed Fouad <ahmedfouad2020era@gmail.com>
2019-05-20 23:42:58 +0200

    Update phone.rb

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,5 +1,5 @@
 class Phone < ApplicationRecord
-	belongs_to :user
-	VALID_PHONE_REGEX = /\A[0-9]+\z/
-	validates :number, length: { minimum: 10, maximum: 10 },format: { with: VALID_PHONE_REGEX }, presence: true, uniqueness: true
+  belongs_to :user
+  VALID_PHONE_REGEX = /\A[0-9]+\z/
+  validates :number, length: { minimum: 10, maximum: 10 },format: { with: VALID_PHONE_REGEX }, presence: true, uniqueness: true
 end
